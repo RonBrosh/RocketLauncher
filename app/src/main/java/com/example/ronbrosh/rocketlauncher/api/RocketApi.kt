d786b34b9b22964adcb08c7769e80f04148a63be
RonBrosh <rbrosh@safetone.com>
2018-12-14 20:37:56 +0000

    Adding http logging.

--- app/src/main/java/com/example/ronbrosh/rocketlauncher/api/RocketApi.kt
+++ app/src/main/java/com/example/ronbrosh/rocketlauncher/api/RocketApi.kt
@@ -27,15 +27,3 @@ interface RocketApi {
         }
     }
 }
-//
-//HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
-//interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
-//OkHttpClient client = new OkHttpClient.Builder().addInterceptor(interceptor).build();
-//
-//Retrofit retrofit = new Retrofit.Builder()
-//.baseUrl("https://backend.example.com")
-//.client(client)
-//.addConverterFactory(GsonConverterFactory.create())
-//.build();
-//
-//return retrofit.create(ApiClient.class);
